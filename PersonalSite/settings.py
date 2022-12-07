4680812297708a24e88afb81487dbadb1aaedd6d
fernandeclercq <fernandomedinadb@hotmail.com>
2022-12-07 21:08:14 +0100

    edit settings.py for deployment to server

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rgapd1qe@6vnm^hnlsl&6j0nq1!_i*(15d&zn@ni^lu-72dk-r'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(environ['IS_DEBUGGING'] in ['True'])
 
 ALLOWED_HOSTS = ['*']
 
