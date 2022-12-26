cc65dd90881d202ab8eb203ab6a6e6fcfcfba91e
fernandeclercq <fernandomedinadb@hotmail.com>
2022-12-26 15:07:04 +0100

    update url routes for post views

--- src/PersonalSite/urls.py
+++ src/PersonalSite/urls.py
@@ -1,15 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from .views import index,blog, post
+from django.urls import path, include
+from posts.views import index, blog, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('blog', blog),
-    path('post', post),
+    path('blog', blog, name="blog/"),
+    path('post', post, name="post/"),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
