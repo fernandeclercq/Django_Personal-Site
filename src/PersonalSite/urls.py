0164fc62d880bff941ef1d9a3e789a4f10ac6529
fernandeclercq <fernandomedinadb@hotmail.com>
2022-12-29 23:16:48 +0100

    modify route path for tinymce

--- src/PersonalSite/urls.py
+++ src/PersonalSite/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-    path(r'^tinymce/', include('tinymce.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 
