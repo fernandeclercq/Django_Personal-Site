57805795a37dd286b26faea7ef8ec22aa326c100
fernandeclercq <fernandomedinadb@hotmail.com>
2022-12-29 21:35:53 +0100

    add tinymce url route

--- src/PersonalSite/urls.py
+++ src/PersonalSite/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
+    path(r'^tinymce/', include('tinymce.urls')),
 ]
 
 
