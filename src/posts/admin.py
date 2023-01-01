c02deb6041c64f288bb9fd8cb477cb78214c3289
fernandeclercq <fernandomedinadb@hotmail.com>
2023-01-01 22:14:19 +0100

    register comment for the admin page

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Category, Post, Author
+from .models import Category, Post, Author, Comment
 
 # Register your models here.
 
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Comment)
 
 
