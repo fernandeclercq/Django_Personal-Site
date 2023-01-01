a685f485c77a00b6956d373e31bee3220c71e558
fernandeclercq <fernandomedinadb@hotmail.com>
2023-01-01 22:02:17 +0100

    modify root path for tinymce

--- src/PersonalSite/settings.py
+++ src/PersonalSite/settings.py
@@ -130,13 +130,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TINYMCE_DEFAULT_CONFIG = {
     "theme": "silver",
-    "height": 500,
-    "menubar": False,
+    "height": 600,
+    "menubar": True,
     "plugins": "advlist,autolink,lists,link,image,charmap,print,preview,anchor,"
     "searchreplace,visualblocks,code,fullscreen,insertdatetime,media,table,paste,"
     "code,help,wordcount",
-    "toolbar": "undo redo | formatselect | "
-    "bold italic backcolor | alignleft aligncenter "
-    "alignright alignjustify | bullist numlist outdent indent | "
-    "removeformat | help",
+    "toolbar1": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat",
+    "toolbar2": "pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
     }
+
+TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, "tinymce")
