70571d84bcd9f6af8d79fd8a14c4928628a7f2f3
fernandeclercq <fernandomedinadb@hotmail.com>
2023-01-01 22:04:38 +0100

    add previous & next post attribute to post model

--- src/posts/models.py
+++ src/posts/models.py
@@ -35,10 +35,14 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
+    previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
 
     def __str__(self):
-        return self.title
+        return "{} - {}".format(
+            self.id, self.title
+            )
 
     
     def get_absolute_url(self):
