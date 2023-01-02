7e3854949c53991ff61eaae484620434299559c6
fernandeclercq <fernandomedinadb@hotmail.com>
2023-01-02 16:41:50 +0100

    update routes for update & delete

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,11 +3,11 @@ from .views import index, blog, post, search, post_update, post_delete, post_cre
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="home"),
     path('blog/', blog, name="post-list"),
     path('post/<int:id>/', post, name="post-detail"),
     path('search/', search, name='post-search'),
     path('post/create/', post_create, name="post-create"),
-    path('post/<int:id>/update', post_update, name="post-update"),
-    path('post/<int:id>/delete', post_delete, name="post-delete"),
+    path('post/<int:id>/update/', post_update, name="post-update"),
+    path('post/<int:id>/delete/', post_delete, name="post-delete"),
 ]
