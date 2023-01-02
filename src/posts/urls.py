5f8a4f77cce2231c054b16163a35e65e4a361680
fernandeclercq <fernandomedinadb@hotmail.com>
2023-01-02 13:01:23 +0100

    add new routes for create, update & delete posts

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index, blog, post, search
+from .views import index, blog, post, search, post_update, post_delete, post_create
 
 
 urlpatterns = [
     path('', index),
     path('blog/', blog, name="post-list"),
     path('post/<int:id>/', post, name="post-detail"),
-    path('search', search, name='post-search'),
+    path('search/', search, name='post-search'),
+    path('post/create/', post_create, name="post-create"),
+    path('post/<int:id>/update', post_update, name="post-update"),
+    path('post/<int:id>/delete', post_delete, name="post-delete"),
 ]
